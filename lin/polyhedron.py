b54a4ba77ea1b084b486563b7abfc15b2511275c
Greg Toombs <gtoombs@lakeheadu.ca>
2019-04-21 20:30:59 -0400

    Fix up some more inspections

--- lin/polyhedron.py
+++ lin/polyhedron.py
@@ -12,16 +12,14 @@ def matrix_combinations(matr, n):
     timed = list(map(list, it.combinations(matr, n)))
     for i in range(n):
         timed[i][i][i] = np.asscalar(timed[i][i][i])
-    all = np.array(list(timed))
-    return all
+    return np.array(list(timed))
 
 
 def array_combinations(arr, n):
     timed = list(map(list, it.combinations(arr, n)))
     for i in range(n):
         timed[i][i] = np.asscalar(timed[i][i])
-    all = np.array(list(timed))
-    return all
+    return np.array(list(timed))
 
 
 def check_extreme(matr, arr, x, sym_comb, m):
@@ -57,7 +55,7 @@ def extreme_points(m, n, A, b, sym_comb):
     # Input
     A = np.array(A).reshape(m, n)
     b = np.array(b).reshape(m, 1)
-    # Proccess
+    # Process
     ans_comb = np.zeros((1, n))
     arr_comb = array_combinations(b, n)
     matr_comb = matrix_combinations(A, n)
@@ -65,13 +63,13 @@ def extreme_points(m, n, A, b, sym_comb):
         if np.linalg.det(matr_comb[i]) != 0:
             x = np.linalg.solve(matr_comb[i], arr_comb[i])
             ans_comb = np.vstack([ans_comb, x])
-    ans_comb = np.delete(ans_comb, (0), axis=0)
+    ans_comb = np.delete(ans_comb, 0, axis=0)
     j = 0
     for i in range(len(ans_comb)):
         if check_extreme(A, b, ans_comb[j], sym_comb, m):
             ans_comb = ans_comb
             j = j + 1
         else:
-            ans_comb = np.delete(ans_comb, (j), axis=0)
+            ans_comb = np.delete(ans_comb, j, axis=0)
     # Output
     return ans_comb
