eb0cb661b9607e0779b5ab6b7a0c4d657cbfabda
Greg Toombs <gtoombs@lakeheadu.ca>
2019-04-21 20:25:37 -0400

    Drop some unnecessary imports

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from setuptools import setup, find_packages
-from os.path import join, dirname
 import lin
 
 setup(
@@ -10,9 +9,7 @@ setup(
     packages=find_packages(),
     author='Andrey Love',
     author_email='r4nd0m1999@gmail.com',
-	install_requires=[
-    	'NumPy==1.15.4'
-	],
+    install_requires=['NumPy'],
     keywords=['linear programming', 'polyhedral set', 'extreme points'],
     url='https://github.com/r4ndompuff/polyhedral_set'
 )
